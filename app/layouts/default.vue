f7435a629e3200c0ddac19927447c2c91812f557
dan-hale <dan@danhale.io>
2026-04-15 15:46:47 -0400

    feat: Extract footer into a server only component

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -96,42 +96,7 @@ const { data: collections } = await useFetch('/api/collections')
         </div>
       </div>
     </div>
-    <footer
-      class="fixed bottom-0 flex h-12 w-screen flex-col items-center justify-between space-y-2 border-t border-gray-400 bg-background px-4 font-sans text-[11px] sm:h-6 sm:flex-row sm:space-y-0"
-    >
-      <div class="flex flex-wrap justify-center space-x-2 pt-2 sm:justify-start">
-        <NuxtLink to="/" class="hover:bg-accent2 hover:underline">
-          Home
-        </NuxtLink>
-        <span>|</span>
-        <span role="link" aria-disabled="true" class="text-muted-foreground">
-          FAQ
-        </span>
-        <span>|</span>
-        <span role="link" aria-disabled="true" class="text-muted-foreground">
-          Returns
-        </span>
-        <span>|</span>
-        <span role="link" aria-disabled="true" class="text-muted-foreground">
-          Careers
-        </span>
-        <span>|</span>
-        <span role="link" aria-disabled="true" class="text-muted-foreground">
-          Contact
-        </span>
-      </div>
-      <div class="text-center sm:text-right">
-        By using this website, you agree to check out the
-        <a
-          href="https://github.com/dan-hale/NuxtFaster"
-          class="font-bold text-accent1 hover:underline"
-          target="_blank"
-          rel="noopener noreferrer"
-        >Source Code</a>
-      </div>
-    </footer>
-    <ClientOnly>
-      <WelcomeToast />
-    </ClientOnly>
+    <LayoutFooter />
+    <WelcomeToast />
   </div>
 </template>
