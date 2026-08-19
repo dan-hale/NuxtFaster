737ca60400218cc1bfe2a3f5b36b4876bb1ffe27
dan hale <dhale2050@gmail.com>
2026-08-19 19:09:22 -0400

    chore: Update database configuration to use Turso with required environment variables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,16 @@
 import { env } from 'node:process'
 import { defineConfig } from 'drizzle-kit'
 
-if (!env.DATABASE_URL)
-  throw new Error('POSTGRES_URL is not set')
+const {TURSO_DATABASE_URL, TURSO_AUTH_TOKEN} = env
+if (!TURSO_DATABASE_URL)   throw new Error('TURSO_DATABASE_URL is required')
+if (!TURSO_AUTH_TOKEN) throw new Error('TURSO_AUTH_TOKEN is required')
 
 export default defineConfig({
   schema: './db/schema.ts',
-  dialect: 'postgresql',
+  dialect: 'turso',
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: TURSO_DATABASE_URL,
+    authToken: TURSO_AUTH_TOKEN,
   },
   verbose: true,
   strict: true,
